29cfe1fea72456c7d0bdc36c564f2551793b7fad
GolubDenis <99288665+GolubDenis@users.noreply.github.com>
2022-06-09 21:45:38 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,6 +4,27 @@
 
 template<typename T, int size>
 class TStack {
-
+ private:
+     T arr[100];
+     int t;
+ public:
+     TStack() :t(-1) { }
+     T get() const {
+         return arr[t];
+     }
+     bool Full() const {
+         return t == razmer - 1;
+     }
+     bool Empty() const {
+         return t == -1;
+     }
+     void pop() {
+         if (t >= 0)
+             t--;
+     }
+     void push(T obj) {
+         if (t < razmer - 1)
+             arr[++t] = obj;
+     }
 };
 #endif  // INCLUDE_TSTACK_H_
